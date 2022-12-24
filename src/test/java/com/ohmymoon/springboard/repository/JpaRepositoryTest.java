1f03509358dc09ab182438ae34df9ceb3f4cb0fe
ohmymoon-216 <ohmymoon@gmail.com>
2022-12-24 10:26:12 +0900

    #10 - 테스트 코드에 new line 추가

--- src/test/java/com/ohmymoon/springboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/ohmymoon/springboard/repository/JpaRepositoryTest.java
@@ -6,11 +6,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
